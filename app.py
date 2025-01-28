ecb09b1699ef1b51f84ce03e09d966ad18a2bc65
danisalexiaa <91732339+danisalexiaa@users.noreply.github.com>
2025-01-28 18:42:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler, Normalizer
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, Normalizer, StandardScaler
 from scipy.stats import kurtosis, skew
 
 # Regresia liniara
